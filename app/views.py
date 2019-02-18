b7cb58f4b5a580935ceffd9bcf1a9ba2e071d18f
Florence <mbababziflorence81@gmail.com>
2019-02-18 13:08:38 +0200

    adding configuration classes in the config.py file

--- app/views.py
+++ app/views.py
@@ -8,4 +8,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'News Source'
+    return render_template('index.html',message = message)
+
+@app.route('/source/<source_id>')
+def source(source_id):
+
+    '''
+    View source page function that returns the source details page and its data
+    '''
+    return render_template('source.html',id = source_id)    
