addd5eca3c40be3dc0cc819b37436f3cc437e771
Florence <mbababziflorence81@gmail.com>
2019-02-18 12:09:23 +0200

    adding run.py file so that i can debugger the content and even to import app form app

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return '<h1> Hello World </h1>'
+from app import app
+
 if __name__ == '__main__':
     app.run(debug = True)
+
+
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route('/')
+# def index():
+#     # return '<h1> Hello World </h1>'
+# if __name__ == '__main__':
+#     app.run(debug = True)
