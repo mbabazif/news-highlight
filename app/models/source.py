572c50e90e3cbe9714059cfda93d43d92f0d875e
Florence <mbababziflorence81@gmail.com>
2019-02-18 16:21:11 +0200

    adding the instances from the api into the source file

---
+++ app/models/source.py
@@ -0,0 +1,12 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,articles,title,name,author,description,url):
+        self.articles = articles
+        self.title = title
+        self.name = name
+        self.author = author
+        self.description = description
+        self.url = url
