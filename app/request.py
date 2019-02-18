499f50604c3f1b229dfb8f38c79ef51a4ab1b02b
Florence <mbababziflorence81@gmail.com>
2019-02-18 18:07:47 +0200

    adding function that returns the index page and it's contents

--- app/request.py
+++ app/request.py
@@ -14,7 +14,7 @@ def get_source(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_movies_url = base_url.format(category,api_key)
+    get_source_url = base_url.format(category,api_key)
 
     with urllib.request.urlopen(get_source_url) as url:
         get_source_data = url.read()
